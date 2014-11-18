2e6cf138e2c83072592992c4b715080aa840eb57
Ain Tohvri <ain@flashbit.net>
2014-11-19 00:52:18 +0100

    Add base structure incl. tests

--- lib/koodmeeter.rb
+++ lib/koodmeeter.rb
@@ -1,2 +1,5 @@
 module Koodmeeter
+  def self.test(password)
+    raise ArgumentError.new 'Password argument required!' if password.nil?
+  end
 end
--- spec/koodmeeter_spec.rb
+++ spec/koodmeeter_spec.rb
@@ -1,6 +1,10 @@
 require 'spec_helper'
 
 describe Koodmeeter do
-  it "should throw error if initialised without parameter" do
+  it "expect to throw error without parameter" do
+    expect{Koodmeeter.test}.to raise_error(ArgumentError)
+  end
+  it "expect to pass test with number for parameter" do
+    expect{Koodmeeter.test(21233)}.not_to raise_error
   end
 end
