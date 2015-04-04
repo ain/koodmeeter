1b8c623d033d9b1850cebe5fe953311e0d502aa9
Ain Tohvri <ain@flashbit.net>
2015-04-05 00:23:03 +0200

    Add axioms

---
+++ lib/koodmeeter/axiom.rb
@@ -0,0 +1,12 @@
+module Koodmeeter
+  AXIOMS = [
+    { :regex => /[a-z]/, :score => 1 },
+    { :regex => /[A-Z]/, :score => 5 },
+    { :regex => /([a-z].*[A-Z])|([A-Z].*[a-z])/, :score => 2 },
+    { :regex => /(.*[0-9].*[0-9].*[0-9])/, :score => 7 },
+    { :regex => /.[!@#$%^&*?_~]/, :score => 5 },
+    { :regex => /(.*[!@#$%^&*?_~].*[!@#$%^&*?_~])/, :score => 7 },
+    { :regex => /([a-zA-Z0-9].*[!@#$%^&*?_~])|([!@#$%^&*?_~].*[a-zA-Z0-9])/, :score => 3 },
+    { :regex => /(.)\1+$/, :score => 2 }
+  ]
+end
