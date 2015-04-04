ab1ecee3a2854f0c8253d268c02679ff215cb58e
Ain Tohvri <ain@flashbit.net>
2015-04-04 23:54:24 +0200

    Add levels and scores constants

--- lib/koodmeeter/core.rb
+++ lib/koodmeeter/core.rb
@@ -1,4 +1,8 @@
 module Koodmeeter
+
+  LEVELS = (0..5).to_a
+  SCORES = [10, 15, 25, 45]
+
   def self.test(password)
     raise ArgumentError.new 'Password argument required!' if password.nil?
     return 0 unless blacklist.index(password.to_s).nil?
