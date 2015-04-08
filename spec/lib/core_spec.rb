ec5dd65f5f0c682f80917a8a95970b921b0aa518
Ain Tohvri <ain@flashbit.net>
2015-04-08 23:45:05 +0200

    Add tests for all levels (0-5)

--- spec/lib/core_spec.rb
+++ spec/lib/core_spec.rb
@@ -31,6 +31,30 @@ describe Koodmeeter do
       subject { Koodmeeter.test(12345) }
       it { is_expected.to eql 0 }
     end
+    context 'with 4 lowercase letters followed by a number' do
+      subject { Koodmeeter.test('asdf1') }
+      it { is_expected.to eql 0 }
+    end
+    context 'with 4 lowercase letters followed by 2 numbers' do
+      subject { Koodmeeter.test('asdf12') }
+      it { is_expected.to eql 1 }
+    end
+    context 'with 4 lowercase letters followed by 3 numbers' do
+      subject { Koodmeeter.test('asdf113') }
+      it { is_expected.to eql 2 }
+    end
+    context 'with 4 lowercase letters followed by 3 numbers and a dash' do
+      subject { Koodmeeter.test('asdf113-') }
+      it { is_expected.to eql 3 }
+    end
+    context 'with 4 lowercase letters followed by 3 numbers, a dash and a dollar' do
+      subject { Koodmeeter.test('asdf113-$') }
+      it { is_expected.to eql 4 }
+    end
+    context 'with 4 lowercase letters followed by 3 numbers, a dash, a dollar and 2 numbers' do
+      subject { Koodmeeter.test('asdf113-$55') }
+      it { is_expected.to eql 5 }
+    end
   end
 
   describe '.blacklist' do
