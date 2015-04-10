8f2cd4981dc292d93adab512f01e6a60afb9191c
Ain Tohvri <ain@flashbit.net>
2015-04-10 22:54:35 +0200

    Update core with wrapper for static public methods

--- lib/koodmeeter/core.rb
+++ lib/koodmeeter/core.rb
@@ -5,50 +5,56 @@ module Koodmeeter
 
   @@scores = [10, 15, 25, 45]
 
-  def self.check(password)
-    password = password.to_s
+  class << self
 
-    raise ArgumentError.new 'Password argument required!' if password.nil?
-    return 0 unless blacklist.index(password).nil?
+    def check(password)
+      password = password.to_s
 
-    length = password.length
-    diff = length - MIN_CHARS
-    scores_dupe = @@scores.dup
-    score = self.calculate_diff_increment(diff)
+      raise ArgumentError.new 'Password argument required!' if password.nil?
+      return 0 unless blacklist.index(password).nil?
 
-    Axiom.list.each do |axiom|
-      if axiom[:regex].match(password)
-        score += axiom[:score]
+      length = password.length
+      diff = length - MIN_CHARS
+      scores_dupe = @@scores.dup
+      score = calculate_diff_increment(diff)
+
+      Axiom.list.each do |axiom|
+        if axiom[:regex].match(password)
+          score += axiom[:score]
+        end
       end
+
+      score += length
+      if score < 0 && score > -199
+        index = 0
+      else
+        scores_dupe.push(score).sort!
+        index = scores_dupe.index(score) + 1
+      end
+
+      return LEVELS[index].nil? ? LEVELS.last : LEVELS[index]
     end
 
-    score += length
-    if score < 0 && score > -199
-      index = 0
-    else
-      scores_dupe.push(score).sort!
-      index = scores_dupe.index(score) + 1
+    def blacklist
+      file = File.read('data/blacklist.json')
+      JSON.parse(file)['blacklist']
     end
 
-    return LEVELS[index].nil? ? LEVELS.last : LEVELS[index]
-  end
+    private
 
-  def self.calculate_diff_increment(diff)
-    score = 0;
-    if diff < 0
-      score -= 100
-    elsif diff >= 5
-      score += 18
-    elsif diff >= 3
-      score += 12
-    elsif diff == 2
-      score += 6
+    def calculate_diff_increment(diff)
+      score = 0;
+      if diff < 0
+        score -= 100
+      elsif diff >= 5
+        score += 18
+      elsif diff >= 3
+        score += 12
+      elsif diff == 2
+        score += 6
+      end
+      return score
     end
-    return score
-  end
 
-  def self.blacklist
-    file = File.read('data/blacklist.json')
-    JSON.parse(file)['blacklist']
   end
 end
